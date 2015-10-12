7374c42c6692c1662f45dabeee36ea4bca013803
Adrian Rangel <adrian.rangel@gmail.com>
2015-10-11 22:09:10 -0500

    fix test not running

--- tests/rfc_facil/test_rfc.rb
+++ tests/rfc_facil/test_rfc.rb
@@ -1,7 +1,7 @@
 class TestRfc < Minitest::Test
-  def should_build_rfc
-    Rfc.new(name: 'Adrian Marcelo', first_last_name: 'Rangel',
-            second_last_name: 'Araujo', day: 27, month: 11, year: 1992)
+  def test_rfc
+    rfc = Rfc.new(name: 'Adrian Marcelo', first_last_name: 'Rangel',
+                  second_last_name: 'Araujo', day: 27, month: 11, year: 1992)
 
     assert_equal(rfc.ten_digits_code, 'RAAA921127')
     assert_equal(rfc.homoclave, 'RI')
