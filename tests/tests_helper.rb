994a7c867843a327dbd32f5c53dddeb73710891c
Adrian Rangel <adrian.rangel@gmail.com>
2015-11-03 21:54:54 -0600

    add codeclimate test coverage

--- tests/tests_helper.rb
+++ tests/tests_helper.rb
@@ -1,6 +1,9 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 require 'minitest/autorun'
 require_relative '../lib/rfc_facil'
 require 'rfc_facil/test_homoclave_calculator'
 require 'rfc_facil/test_rfc'
 require 'rfc_facil/test_ten_digits_code_calculator'
 require 'rfc_facil/test_verification_digit_calculator'
+
